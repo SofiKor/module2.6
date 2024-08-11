a565731745af5dddbf3bedaec1387db8c096f7ab
SofiKor <sofiakorelskaya@mail.ru>
2024-08-11 17:57:06 +0300

    Add files via upload

--- module2.6.py
+++ module2.6.py
@@ -1,8 +1,8 @@
 def generate_password(n):
     result = ""
     for i in range(1, n):
-        for j in range(i+1, n):
-            if (i + j) % n == 0:
+        for j in range(i + 1, n):
+            if n % (i + j) == 0:
                 result += str(i) + str(j)
     return result
 
@@ -13,3 +13,4 @@ while number < 3 or number > 20:
 
 password = generate_password(number)
 print(f'Пароль для числа: {password}')
+
